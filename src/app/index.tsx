3e53bddd9d1e57ca7e1df0c2b8e6b598172b40d9
Will Zhou <iwillzhou@outlook.com>
2024-11-04 16:35:47 +0800

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>NODE_ENV: {process.env.NODE_ENV}</Text>
+      <Text>API_URL: {process.env.API_URL}</Text>
+      <Text>EXPO_PUBLIC_API_URL: {process.env.EXPO_PUBLIC_API_URL}</Text>
+    </View>
+  );
+}

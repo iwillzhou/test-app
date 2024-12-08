5359616cd0fdcb3f4fe9ce3d44c38169aca10758
Will Zhou <iwillzhou@outlook.com>
2024-12-09 01:51:26 +0800

    action

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -21,14 +21,13 @@ export default function Index() {
             style={{
                 flex: 1,
                 justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'red'
+                alignItems: 'center'
             }}
         >
-            <Text>Hello world. OTA updates is working! </Text>
+            <Text>Hello world. </Text>
             <Text>Edit app/index.tsx to edit this screen.</Text>
             <Text>NODE_ENV: {process.env.NODE_ENV}</Text>
-            <Text>API_URL: {process.env.API_URL}</Text>
+            <Text>APP_ENV: {process.env.APP_ENV}</Text>
             <Text>EXPO_PUBLIC_API_URL: {process.env.EXPO_PUBLIC_API_URL}</Text>
             <Button title="Fetch update" onPress={onFetchUpdateAsync} />
         </View>
